c53bf72b7af278115b6eac645b7ab37eb9af1a47
listening <893979653@qq.com>
2016-06-06 23:45:14 +0800

    创建方法

--- class/customer.rb
+++ class/customer.rb
@@ -1,16 +1,25 @@
 class Customer
   @@no_of_customers = 0
-
   def initialize(id, name, addr)
     @cust_id = id
     @cust_name = name
     @cust_addr = addr
   end
-
+  def display_details()
+    puts "Customer id #@cust_id"
+    puts "Customer name #@cust_name"
+    puts "Customer addr #@cust_addr"
+  end
+  def total_no_of_customers()
+    @@no_of_customers += 1
+    puts "Total number of customers: #@@no_of_customers"
+  end
   def hello
     puts "Hello Customer!"
   end
 end
 
 cust1 = Customer.new("1", "listening", "shenzhen")
-cust1.hello
+#cust1.hello
+#cust1.display_details
+cust1.total_no_of_customers
